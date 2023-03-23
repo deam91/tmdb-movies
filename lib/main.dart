2fc05fe23e1a52b17c2f029d4307a41b08df2cb5
ae-deam <deam@aleph.engineering>
2023-03-23 18:08:18 -0400

    ref: main

--- lib/main.dart
+++ lib/main.dart
@@ -1,66 +1,76 @@
+import 'dart:async';
+
+import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:hive_flutter/adapters.dart';
+import 'package:loggy/loggy.dart';
+import 'package:movies_tmdb/src/core/controllers/providers/app_route_provider.dart';
+import 'package:movies_tmdb/src/core/controllers/providers/dio_provider.dart';
+import 'package:movies_tmdb/src/core/controllers/providers/theme_provider.dart';
+import 'package:movies_tmdb/src/dashboard/controllers/controllers/providers.dart';
 
 void main() {
-  runApp(const MyMoviesApp());
+  unawaited(runZonedGuarded<Future<void>>(() async {
+    WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+    FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+
+    /// We can use multiple flavors with multiple environment files.
+    /// We do not use it here for the sake of the example
+    await dotenv.load(fileName: "assets/env/.env");
+
+    /// Firebase init & Crashlytics errors logging
+    // await Firebase.initializeApp(
+    //   options: DefaultFirebaseOptions.currentPlatform,
+    // );
+    // FlutterError.onError =
+    //     FirebaseCrashlytics.instance.recordFlutterFatalError;
+
+    // console logging
+    Loggy.initLoggy(
+      logPrinter: const PrettyPrinter(),
+      logOptions: const LogOptions(
+        LogLevel.all,
+      ),
+    );
+
+    // local storage
+    await Hive.initFlutter();
+
+    final container = ProviderContainer();
+    container.read(dioProvider);
+    container.read(moviesRepositoryProvider);
+
+    runApp(UncontrolledProviderScope(
+      container: container,
+      child: const MyMoviesApp(),
+    ));
+
+    FlutterNativeSplash.remove();
+  }, (error, stack) => print(error)
+      // FirebaseCrashlytics.instance.recordError(error, stack, fatal: true),
+      ));
 }
 
-class MyMoviesApp extends StatelessWidget {
+class MyMoviesApp extends ConsumerWidget {
   const MyMoviesApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'TV Shows'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+  Widget build(BuildContext context, WidgetRef ref) {
+    final palette = ref.read(themeProvider);
+    final appRouter = ref.read(appRouterProvider);
+    return MaterialApp.router(
+      routerConfig: appRouter.config(),
+      title: 'Movies Demo',
+      theme: palette.darkTheme.copyWith(
+        pageTransitionsTheme: const PageTransitionsTheme(
+          builders: {
+            TargetPlatform.iOS: NoShadowCupertinoPageTransitionsBuilder(),
+            TargetPlatform.android: FadeUpwardsPageTransitionsBuilder(),
+          },
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ),
     );
   }
