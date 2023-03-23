e224875483ed45d45af0127c1c5339ec1984b39c
ae-deam <deam@aleph.engineering>
2023-03-23 18:06:08 -0400

    feat: add movies providers

---
+++ lib/src/dashboard/controllers/controllers/providers.dart
@@ -0,0 +1,53 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:movies_tmdb/src/core/controllers/providers/dio_provider.dart';
+import 'package:movies_tmdb/src/core/models/api/api.dart';
+import 'package:movies_tmdb/src/core/models/data_models/movie.dart';
+import 'package:movies_tmdb/src/core/models/data_models/movie_detail.dart';
+import 'package:movies_tmdb/src/dashboard/models/movie_repository.dart';
+import 'package:movies_tmdb/src/dashboard/models/movie_repository_local.dart';
+import 'package:movies_tmdb/src/dashboard/models/movie_repository_remote.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+import '../../../core/controllers/providers/connection_provider.dart';
+
+part 'providers.g.dart';
+
+@riverpod
+MoviesRepository moviesRepository(MoviesRepositoryRef ref) {
+  final isOnline = ref.watch(networkConnectivityProvider);
+  final dio = ref.read(dioProvider);
+  final baseUrl = dotenv.get('BASE_URL');
+  final apiKey = dotenv.get('API_KEY');
+  final restClient = TMDBRestClient(dio, baseUrl: baseUrl);
+  return MoviesRepository(
+    restClient: restClient,
+    remoteMovieRepository:
+        RemoteMovieRepository(dioClient: dio, apiKey: apiKey),
+    localMovieRepository: LocalMovieRepository(),
+    isOnline: isOnline.state == ConnectivityStatus.isConnected ? true : false,
+  );
+}
+
+@riverpod
+Future<MovieDetail> movie(
+  MovieRef ref, {
+  required int movieId,
+}) {
+  return ref.read(moviesRepositoryProvider).movie(movieId: movieId);
+}
+
+@riverpod
+Future<List<Movie>> popularMovies(
+  PopularMoviesRef ref, {
+  required int? page,
+}) {
+  return ref.read(moviesRepositoryProvider).popularMovies(page: page);
+}
+
+@riverpod
+Future<List<Movie>> topRatedMovies(
+  TopRatedMoviesRef ref, {
+  required int? page,
+}) {
+  return ref.read(moviesRepositoryProvider).topRatedMovies(page: page);
+}
---
+++ lib/src/dashboard/controllers/controllers/providers.g.dart
@@ -0,0 +1,290 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'providers.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$moviesRepositoryHash() => r'f20a55bcba0e338316075b3d0b839b206aeadcc7';
+
+/// See also [moviesRepository].
+@ProviderFor(moviesRepository)
+final moviesRepositoryProvider = AutoDisposeProvider<MoviesRepository>.internal(
+  moviesRepository,
+  name: r'moviesRepositoryProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$moviesRepositoryHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef MoviesRepositoryRef = AutoDisposeProviderRef<MoviesRepository>;
+String _$movieHash() => r'bfc8cd1aea34499188c737d17e66e1ed1235a7e4';
+
+/// Copied from Dart SDK
+class _SystemHash {
+  _SystemHash._();
+
+  static int combine(int hash, int value) {
+    // ignore: parameter_assignments
+    hash = 0x1fffffff & (hash + value);
+    // ignore: parameter_assignments
+    hash = 0x1fffffff & (hash + ((0x0007ffff & hash) << 10));
+    return hash ^ (hash >> 6);
+  }
+
+  static int finish(int hash) {
+    // ignore: parameter_assignments
+    hash = 0x1fffffff & (hash + ((0x03ffffff & hash) << 3));
+    // ignore: parameter_assignments
+    hash = hash ^ (hash >> 11);
+    return 0x1fffffff & (hash + ((0x00003fff & hash) << 15));
+  }
+}
+
+typedef MovieRef = AutoDisposeFutureProviderRef<MovieDetail>;
+
+/// See also [movie].
+@ProviderFor(movie)
+const movieProvider = MovieFamily();
+
+/// See also [movie].
+class MovieFamily extends Family<AsyncValue<MovieDetail>> {
+  /// See also [movie].
+  const MovieFamily();
+
+  /// See also [movie].
+  MovieProvider call({
+    required int movieId,
+  }) {
+    return MovieProvider(
+      movieId: movieId,
+    );
+  }
+
+  @override
+  MovieProvider getProviderOverride(
+    covariant MovieProvider provider,
+  ) {
+    return call(
+      movieId: provider.movieId,
+    );
+  }
+
+  static const Iterable<ProviderOrFamily>? _dependencies = null;
+
+  @override
+  Iterable<ProviderOrFamily>? get dependencies => _dependencies;
+
+  static const Iterable<ProviderOrFamily>? _allTransitiveDependencies = null;
+
+  @override
+  Iterable<ProviderOrFamily>? get allTransitiveDependencies =>
+      _allTransitiveDependencies;
+
+  @override
+  String? get name => r'movieProvider';
+}
+
+/// See also [movie].
+class MovieProvider extends AutoDisposeFutureProvider<MovieDetail> {
+  /// See also [movie].
+  MovieProvider({
+    required this.movieId,
+  }) : super.internal(
+          (ref) => movie(
+            ref,
+            movieId: movieId,
+          ),
+          from: movieProvider,
+          name: r'movieProvider',
+          debugGetCreateSourceHash:
+              const bool.fromEnvironment('dart.vm.product')
+                  ? null
+                  : _$movieHash,
+          dependencies: MovieFamily._dependencies,
+          allTransitiveDependencies: MovieFamily._allTransitiveDependencies,
+        );
+
+  final int movieId;
+
+  @override
+  bool operator ==(Object other) {
+    return other is MovieProvider && other.movieId == movieId;
+  }
+
+  @override
+  int get hashCode {
+    var hash = _SystemHash.combine(0, runtimeType.hashCode);
+    hash = _SystemHash.combine(hash, movieId.hashCode);
+
+    return _SystemHash.finish(hash);
+  }
+}
+
+String _$popularMoviesHash() => r'029a14a07aa21109a7f643c25e8efbb14f64d5b5';
+typedef PopularMoviesRef = AutoDisposeFutureProviderRef<List<Movie>>;
+
+/// See also [popularMovies].
+@ProviderFor(popularMovies)
+const popularMoviesProvider = PopularMoviesFamily();
+
+/// See also [popularMovies].
+class PopularMoviesFamily extends Family<AsyncValue<List<Movie>>> {
+  /// See also [popularMovies].
+  const PopularMoviesFamily();
+
+  /// See also [popularMovies].
+  PopularMoviesProvider call({
+    required int? page,
+  }) {
+    return PopularMoviesProvider(
+      page: page,
+    );
+  }
+
+  @override
+  PopularMoviesProvider getProviderOverride(
+    covariant PopularMoviesProvider provider,
+  ) {
+    return call(
+      page: provider.page,
+    );
+  }
+
+  static const Iterable<ProviderOrFamily>? _dependencies = null;
+
+  @override
+  Iterable<ProviderOrFamily>? get dependencies => _dependencies;
+
+  static const Iterable<ProviderOrFamily>? _allTransitiveDependencies = null;
+
+  @override
+  Iterable<ProviderOrFamily>? get allTransitiveDependencies =>
+      _allTransitiveDependencies;
+
+  @override
+  String? get name => r'popularMoviesProvider';
+}
+
+/// See also [popularMovies].
+class PopularMoviesProvider extends AutoDisposeFutureProvider<List<Movie>> {
+  /// See also [popularMovies].
+  PopularMoviesProvider({
+    required this.page,
+  }) : super.internal(
+          (ref) => popularMovies(
+            ref,
+            page: page,
+          ),
+          from: popularMoviesProvider,
+          name: r'popularMoviesProvider',
+          debugGetCreateSourceHash:
+              const bool.fromEnvironment('dart.vm.product')
+                  ? null
+                  : _$popularMoviesHash,
+          dependencies: PopularMoviesFamily._dependencies,
+          allTransitiveDependencies:
+              PopularMoviesFamily._allTransitiveDependencies,
+        );
+
+  final int? page;
+
+  @override
+  bool operator ==(Object other) {
+    return other is PopularMoviesProvider && other.page == page;
+  }
+
+  @override
+  int get hashCode {
+    var hash = _SystemHash.combine(0, runtimeType.hashCode);
+    hash = _SystemHash.combine(hash, page.hashCode);
+
+    return _SystemHash.finish(hash);
+  }
+}
+
+String _$topRatedMoviesHash() => r'bc4fab47d1d0f158c0a4c1878755ef9641f1bf47';
+typedef TopRatedMoviesRef = AutoDisposeFutureProviderRef<List<Movie>>;
+
+/// See also [topRatedMovies].
+@ProviderFor(topRatedMovies)
+const topRatedMoviesProvider = TopRatedMoviesFamily();
+
+/// See also [topRatedMovies].
+class TopRatedMoviesFamily extends Family<AsyncValue<List<Movie>>> {
+  /// See also [topRatedMovies].
+  const TopRatedMoviesFamily();
+
+  /// See also [topRatedMovies].
+  TopRatedMoviesProvider call({
+    required int? page,
+  }) {
+    return TopRatedMoviesProvider(
+      page: page,
+    );
+  }
+
+  @override
+  TopRatedMoviesProvider getProviderOverride(
+    covariant TopRatedMoviesProvider provider,
+  ) {
+    return call(
+      page: provider.page,
+    );
+  }
+
+  static const Iterable<ProviderOrFamily>? _dependencies = null;
+
+  @override
+  Iterable<ProviderOrFamily>? get dependencies => _dependencies;
+
+  static const Iterable<ProviderOrFamily>? _allTransitiveDependencies = null;
+
+  @override
+  Iterable<ProviderOrFamily>? get allTransitiveDependencies =>
+      _allTransitiveDependencies;
+
+  @override
+  String? get name => r'topRatedMoviesProvider';
+}
+
+/// See also [topRatedMovies].
+class TopRatedMoviesProvider extends AutoDisposeFutureProvider<List<Movie>> {
+  /// See also [topRatedMovies].
+  TopRatedMoviesProvider({
+    required this.page,
+  }) : super.internal(
+          (ref) => topRatedMovies(
+            ref,
+            page: page,
+          ),
+          from: topRatedMoviesProvider,
+          name: r'topRatedMoviesProvider',
+          debugGetCreateSourceHash:
+              const bool.fromEnvironment('dart.vm.product')
+                  ? null
+                  : _$topRatedMoviesHash,
+          dependencies: TopRatedMoviesFamily._dependencies,
+          allTransitiveDependencies:
+              TopRatedMoviesFamily._allTransitiveDependencies,
+        );
+
+  final int? page;
+
+  @override
+  bool operator ==(Object other) {
+    return other is TopRatedMoviesProvider && other.page == page;
+  }
+
+  @override
+  int get hashCode {
+    var hash = _SystemHash.combine(0, runtimeType.hashCode);
+    hash = _SystemHash.combine(hash, page.hashCode);
+
+    return _SystemHash.finish(hash);
+  }
+}
+// ignore_for_file: unnecessary_raw_strings, subtype_of_sealed_class, invalid_use_of_internal_member, do_not_use_environment, prefer_const_constructors, public_member_api_docs, avoid_private_typedef_functions
